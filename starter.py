6ca65081be6e212d2435ee8216b1991daac8aca5
Joshua Olatunji <kayodejoshua169@yahoo.com>
2024-10-28 23:40:03 +0100

    1. from settings get TRACE_MEMORY_ALLOCATION_PER_FRAME
    2. seperated the urls file

--- starter.py
+++ starter.py
@@ -4,37 +4,20 @@ import tracemalloc
 
 from tortoise import Tortoise, run_async
 
-from application.upload_use_case import UploadUseCase
-from infrastructure.adapters.db_file_repository import DBFile
-from infrastructure.settings import TORTOISE_ORM
-from infrastructure.web.handlers.file_upload_handler import FileUploadHandler
-from infrastructure.web.handlers.websocket_handler import ProgressWebSocketHandler
-from infrastructure.adapters.websocket_progress_notifier import WebSocketProgressNotifier
+from infrastructure.settings import TORTOISE_ORM, settings
+from infrastructure.web.urls import routes
 
 # Start tracing memory allocations, storing up to 10 frames per allocation
-tracemalloc.start(10)
+tracemalloc.start(settings.TRACE_MEMORY_ALLOCATION_PER_FRAME)
 
 async def db_init():
     await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas()
+    # NOTE: is this a good practice
 
 
 def app():
-    file_repo = DBFile()
-    progress_notifier = WebSocketProgressNotifier(ProgressWebSocketHandler)
-    upload_use_case = UploadUseCase(file_repo, progress_notifier)
-
-    # Return the Tornado application with the following routes:
-    # - Redirect from root ("/") to the static HTML file (index.html)
-    # - "/upload" for file uploads (handled by FileUploadHandler)
-    # - "/ws/progress" for WebSocket connections to notify clients of progress (handled by ProgressWebSocketHandler)
-    # - "/static" for serving static files like HTML, CSS, and JS
-    return tornado.web.Application([
-        (r"/", tornado.web.RedirectHandler, {"url": "/static/index.html"}),
-        (r"/upload", FileUploadHandler, dict(upload_use_case=upload_use_case)),
-        (r"/ws/progress", ProgressWebSocketHandler),
-        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "./static"}),
-    ])
+    return tornado.web.Application(routes)
 
 if __name__ == "__main__":
     run_async(db_init())
