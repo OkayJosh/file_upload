f09fe8b4e97f675bad13671125e9a0b29d43d836
Joshua Olatunji <kayodejoshua169@yahoo.com>
2024-10-28 23:08:53 +0100

    1. Make the service async: due to the fact that tortoise orm is async by default
    2. included _upload_chunk to have more control of the individual chunk i.e awaiting each chuck upload within the for loop

--- domain/service.py
+++ domain/service.py
@@ -13,6 +13,12 @@ Example Use Case:
     - Uploading a large file in smaller chunks to prevent memory overload,
       while keeping the user informed of the progress.
 """
+from typing import TypeVar
+
+_F = TypeVar('_F', bound='FileEntity')
+_T = TypeVar('_T', bound='FileRepository')
+_N = TypeVar('_N', bound='ProgressNotifier')
+
 
 class FileService:
     """
@@ -31,25 +37,25 @@ class FileService:
 
     UPLOAD_RANGE = 10  # The number of chunks to divide the file into during upload.
 
-    def __init__(self, file_repo, progress_notifier):
+    def __init__(self, file_repo: _T, progress_notifier: _N):
         """
         Initialize the FileService with the necessary dependencies.
 
         Args:
-            file_repo: The file repository instance that will handle file storage.
-            progress_notifier: The progress notifier instance to notify upload progress.
+            file_repo(_T): The file repository instance that will handle file storage.
+            progress_notifier(_N): The progress notifier instance to notify upload progress.
         """
         self.file_repo = file_repo
         self.progress_notifier = progress_notifier
 
-    def upload_file(self, file_entity):
+    async def upload_file(self, file_entity: _F) -> None:
         """
         Uploads a file in chunks to the file repository. This method divides the
         file content into smaller chunks, saves each chunk using the repository,
         and notifies the progress notifier after each chunk is uploaded.
 
         Args:
-            file_entity (FileEntity): The file entity containing the file's metadata and content
+            file_entity (_F): The file entity containing the file's metadata and content
                                       that needs to be uploaded.
 
         Raises:
@@ -61,9 +67,31 @@ class FileService:
         uploaded_size = 0
 
         for i in range(self.UPLOAD_RANGE):
-            # Write file in chunks using repository adapter.
-            self.file_repo.save_file_chunk(file_entity, uploaded_size, chunk_size)
-            uploaded_size += chunk_size  # Update the uploaded size.
+            uploaded_size += await self._upload_chunk(
+                file_entity, uploaded_size, chunk_size, i
+                )
 
-            # Notify progress via notifier adapter.
-            self.progress_notifier.notify_progress(f"{(i + 1) * self.UPLOAD_RANGE}%")
+    async def _upload_chunk(self, file_entity: _F,
+                            uploaded_size: int, chunk_size: int,
+                            chunk_index: int) -> int:
+        """
+        Handles the upload of a single chunk of the file and notifies progress.
+
+        Args:
+            file_entity (_F): The file entity being uploaded.
+            uploaded_size (int): The cumulative size of uploaded data.
+            chunk_size (int): The size of the current chunk.
+            chunk_index (int): The index of the current chunk.
+
+        Returns:
+            int: The updated uploaded size after processing the chunk.
+        """
+
+        await self.file_repo.save_file_chunk(
+            file_entity, uploaded_size, chunk_size
+        )
+        uploaded_size += chunk_size
+
+        # Notify progress via notifier adapter.
+        self.progress_notifier.notify_progress(f"{(chunk_index + 1) * self.UPLOAD_RANGE}%")
+        return uploaded_size
